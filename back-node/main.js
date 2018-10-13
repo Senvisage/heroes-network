66d648328bc418d6d44d3dcecf139ff9c4da5954
LoicBrassart <loic.brassart@gmail.com>
2018-10-13 21:45:30 +0200

    Added CORS support, removed (for now) server fumbles, fixed /heroes/:id route.

--- back-node/main.js
+++ back-node/main.js
@@ -35,13 +35,23 @@ app
   // ----- MIDDLEWARES
   // Every once in a while, the server goes MIA (500), else we keep as planned
   .use(function(req, res, next) {
-    // On a fumble (10% probability)
-    if (Math.floor(Math.random() * 20) + 1 <= 2) {
+    // On a fumble
+    if (Math.floor(Math.random() * 20) + 1 <= 0) {
       res.setHeader('Content-Type', 'text/plain');
       res.status(500).send('Server hiccup !');
     } else {
       next();
     }
+  })
+  // One middleware to take care of CORS
+  .options('/*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Access-Control-Allow-Origin, Content-Type, Authorization, Content-Length, X-Requested-With'
+    );
+    res.status(200).send();
   })
   // ----- ROUTING
   .get('/heroes', function(req, res) {
@@ -49,8 +59,8 @@ app
     res.setHeader('Content-Type', 'application/json');
     res.status(200).send(JSON.stringify(heroesData));
   })
-  .get('/heroes/:id-:name', function(req, res) {
-    console.log(`Request for hero #${req.params.id} (${req.params.name})`);
+  .get('/heroes/:id', function(req, res) {
+    console.log(`Request for hero #${req.params.id}`);
     if (heroesData[req.params.id] !== undefined) {
       res.setHeader('Content-Type', 'application/json');
       res.status(200).send(JSON.stringify(heroesData[req.params.id]));
