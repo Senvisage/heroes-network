6ba778480623dd8bc604350fb8308009f5639dd7
LoicBrassart <loic.brassart@gmail.com>
2018-10-13 23:25:47 +0200

    Adding React-router module to ease navigation.

--- front-react/src/App.js
+++ front-react/src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import MyNavbar from './components/MyNavbar';
 import Header from './components/Header';
+import Home from './components/Home';
+import Blog from './components/Blog';
+import HeroDetails from './components/HeroDetails';
 import HeroCollection from './components/HeroCollection';
 
 const DEFAULT_STATE = {
@@ -12,7 +16,7 @@ const DEFAULT_STATE = {
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = DEFAULT_STATE;
+    this.state = { ...DEFAULT_STATE };
     this.changeFilterValue = this.changeFilterValue.bind(this);
   }
 
@@ -25,7 +29,18 @@ class App extends Component {
       <div className="App">
         <MyNavbar onChangeFilterValue={this.changeFilterValue} />
         <Header />
-        <HeroCollection filter={this.state.filter} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/heroes/Scene" component={HeroDetails} />
+          <Route exact path="/blog" component={Blog} />
+          <Route
+            exact
+            path="/heroes"
+            render={props => (
+              <HeroCollection {...props} filter={this.state.filter} />
+            )}
+          />
+        </Switch>
       </div>
     );
   }
