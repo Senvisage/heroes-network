0e50969ff3144172ef4b8b255c1da1618697d059
LoicBrassart <loic.brassart@gmail.com>
2018-10-13 21:43:47 +0200

    Removed sample data from App.js

--- front-react/src/App.js
+++ front-react/src/App.js
@@ -5,26 +5,6 @@ import MyNavbar from './components/MyNavbar';
 import Header from './components/Header';
 import HeroCollection from './components/HeroCollection';
 
-let heroesData = [
-  {
-    id: 1,
-    name: 'Scene',
-    class: 'Bard',
-    race: 'Tiefling',
-    level: 5,
-    pictureUrl:
-      'http://www.d20radio.com/main/wp-content/uploads/2017/01/TieflingBard.jpg'
-  },
-  {
-    id: 2,
-    name: 'Tourment',
-    class: 'Templar',
-    race: 'Tiefling',
-    level: 4,
-    pictureUrl: 'https://i.imgur.com/yWzN0Vj.png'
-  }
-];
-
 const DEFAULT_STATE = {
   filter: ''
 };
@@ -45,7 +25,7 @@ class App extends Component {
       <div className="App">
         <MyNavbar onChangeFilterValue={this.changeFilterValue} />
         <Header />
-        <HeroCollection filter={this.state.filter} heroesData={heroesData} />
+        <HeroCollection filter={this.state.filter} />
       </div>
     );
   }
