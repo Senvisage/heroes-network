d086ec1ed76f5a2727de36554578fbfa040b1644
LoicBrassart <loic.brassart@gmail.com>
2018-10-13 23:26:22 +0200

    Adding new pages for the app.

---
+++ front-react/src/components/Blog.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Blog extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <p>TODO: Blog Component</p>;
+  }
+}
+export default Blog;
---
+++ front-react/src/components/HeroDetails.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class HeroDetails extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <p>TODO: HeroDetails Component</p>;
+  }
+}
+export default HeroDetails;
